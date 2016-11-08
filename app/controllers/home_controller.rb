db2622c7a8e14bdb9fed3d0ff8483a2009aefe6c
Aaron1515 <aaron1515@gmail.com>
2016-11-08 12:20:02 -0800

    init devise

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+
+
+  def index
+  	# if signed in render page
+  	# else
+  	# 	go to sigin in or register page
+  	# end
+  end
+
+  def new
+
+  end
+
+
+end
